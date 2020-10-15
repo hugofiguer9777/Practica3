ec668d2be3ab115a7a114452dfa7f07c6384a439
hugofiguer9777 <58827958+hugofiguer9777@users.noreply.github.com>
2020-10-14 18:27:32 -0600

    Agregar archivos de proyecto.

---
+++ Practica3/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using SimioAPI.Graphics;
+using Simio;
+using Simio.SimioEnums;
+using SimioAPI;
+
+namespace Practica3
+{
+    public partial class Form1 : Form
+    {
+        private ISimioProject proyectoApi;
+        private string rutabase = "ModeloBase.spfx";
+        private string rutafinal = "ModeloFinal.spfx";
+        private string[] warnings;
+        private IModel modelo;
+        private IIntelligentObjects intelligentObjects;
+
+        public Form1()
+        {
+            proyectoApi = SimioProjectFactory.LoadProject(rutabase, out warnings);
+            modelo = proyectoApi.Models[1];
+            intelligentObjects = modelo.Facility.IntelligentObjects;
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            crearModelo();
+        }
+
+        public void crearModelo()
+        {
+            //intelligentObjects.CreateObject("Source", new FacilityLocation(0, 0, 10));
+            //intelligentObjects.CreateObject("Source", new FacilityLocation(0, 0, 15));
+            crearSource("Ejemplo1", 0, 10);
+            crearSource("Ejemplo2", 0, 15);
+
+            //modelo.Facility.IntelligentObjects["Source1"].Properties["InterarrivalTime"].Value = "Random.Exponential(5)";
+
+            SimioProjectFactory.SaveProject(proyectoApi, rutafinal, out warnings);
+        }
+
+        public void crearSource(string nombre, double x, double y)
+        {
+            crearObjeto("Source", x, y);
+            modelo.Facility.IntelligentObjects["Source1"].ObjectName = nombre;
+        }
+
+        public void crearServer(string nombre, double x, double y)
+        {
+            crearObjeto("Server", x, y);
+            modelo.Facility.IntelligentObjects["Server1"].ObjectName = nombre;
+        }
+
+        public void crearSink(string nombre, double x, double y)
+        {
+            crearObjeto("Sink", x, y);
+            modelo.Facility.IntelligentObjects["Sink1"].ObjectName = nombre;
+        }
+
+        public void crearEntity(string nombre, double x, double y)
+        {
+            crearObjeto("ModelEntity", x, y);
+            modelo.Facility.IntelligentObjects["ModelEntity1"].ObjectName = nombre;
+        }
+
+        public void crearTransferNode(string nombre, double x, double y)
+        {
+            crearObjeto("TransferNode", x, y);
+            modelo.Facility.IntelligentObjects["TransferNode1"].ObjectName = nombre;
+        }
+
+        public void crearObjeto(string tipo, double x, double y)
+        {
+            intelligentObjects.CreateObject(tipo, new FacilityLocation(x, 0, y));
+        }
+    }
+}
