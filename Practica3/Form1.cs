198589a013788f53e0af9d0f83206b434962fa46
hugofiguer9777 <58827958+hugofiguer9777@users.noreply.github.com>
2020-10-15 20:47:37 -0600

    Pasando paths a conveyors

--- Practica3/Form1.cs
+++ Practica3/Form1.cs
@@ -23,6 +23,7 @@ namespace Practica3
         private string[] warnings;
         int contNodos = 1;
         int contPaths = 0;
+        int contConveyor = 0;
         private IModel modelo;
         private IIntelligentObjects intelligentObjects;
 
@@ -87,6 +88,12 @@ namespace Practica3
             contPaths++;
         }
 
+        public void crearConveyor(string nodo1, string nodo2)
+        {
+            intelligentObjects.CreateLink("Conveyor", ((INodeObject)modelo.Facility.IntelligentObjects[nodo1]), ((INodeObject)modelo.Facility.IntelligentObjects[nodo2]), null);
+            contConveyor++;
+        }
+
         public void crearObjeto(string tipo, double x, double y)
         {
             intelligentObjects.CreateObject(tipo, new FacilityLocation(x, 0, y));
@@ -109,102 +116,103 @@ namespace Practica3
             // --------- FRONTERA BELICE ---------
             crearTransferNode("N" + contNodos, 42000, -30000);
             crearTransferNode("N" + contNodos, 30000, -30000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 34000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 34000);
 
             crearTransferNode("N" + contNodos, 30000, -80000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 232000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 232000);
 
             // --------- FRONTERA MEXICO ---------
             crearTransferNode("N" + contNodos, -20000, -80000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 210000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 210000);
 
             crearTransferNode("N" + contNodos, -20000, -60000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 83000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 83000);
 
             crearTransferNode("N" + contNodos, -41000, -60000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 57000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 57000);
 
             crearTransferNode("N" + contNodos, -27000, -49000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 79000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 79000);
 
             crearTransferNode("N" + contNodos, -11000, -40000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 86000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 86000);
 
             crearTransferNode("N" + contNodos, -11250, -30000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 46000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 46000);
 
             crearTransferNode("N" + contNodos, -52000, -30000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 151000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 151000);
 
             crearTransferNode("N" + contNodos, -63750, -7500);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 125000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 125000);
 
             crearTransferNode("N" + contNodos, -59000, 2000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 40000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 40000);
 
             crearTransferNode("N" + contNodos, -66000, 18000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 85000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 85000);
 
             // --------- FRONTERA PACIFICO ---------
             crearTransferNode("N" + contNodos, -54000, 36000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 60000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 60000);
 
             crearTransferNode("N" + contNodos, -30000, 43000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 75000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 75000);
 
             crearTransferNode("N" + contNodos, -3000, 40000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 63000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 63000);
 
             crearTransferNode("N" + contNodos, 22500, 42500);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 56000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 56000);
 
             // --------- FRONTERA EL SALVADOR ---------
             crearTransferNode("N" + contNodos, 40000, 16250);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 203000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 203000);
 
             // --------- FRONTERA HONDURAS ---------
             crearTransferNode("N" + contNodos, 50000, 4000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 63000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 63000);
 
             crearTransferNode("N" + contNodos, 47000, -12000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 55000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 55000);
 
             crearTransferNode("N" + contNodos, 60000, -29000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 138000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 138000);
 
             // --------- FRONTERA CARIBE ---------
             crearTransferNode("N" + contNodos, 48500, -26000);
-            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
-            distanciaPath("Path" + contPaths, 96000);
+            crearConveyor("N" + (contNodos - 2), "N" + (contNodos - 1));
+            distanciaConveyor("Conveyor" + contConveyor, 96000);
 
-            crearPath("N" + (contNodos - 1), "N" + (contNodos - 22));
-            distanciaPath("Path" + contPaths, 52000);
+            crearConveyor("N" + (contNodos - 1), "N" + (contNodos - 22));
+            distanciaConveyor("Conveyor" + contConveyor, 52000);
         }
 
-        public void distanciaPath(string nombre, int distancia)
+        public void distanciaConveyor(string nombre, int distancia)
         {
             modelo.Facility.IntelligentObjects[nombre].Properties["DrawnToScale"].Value = "False";
             modelo.Facility.IntelligentObjects[nombre].Properties["LogicalLength"].Value = distancia.ToString();
+            modelo.Facility.IntelligentObjects[nombre].Properties["InitialDesiredSpeed"].Value = 60;
         }
     }
 }
