620e8908b7f20f3c3f5f92ceac7a839ca6b005e0
hugofiguer9777 <58827958+hugofiguer9777@users.noreply.github.com>
2020-10-17 20:50:18 -0600

    Metodos para dibujar carnets

--- Practica3/Form1.cs
+++ Practica3/Form1.cs
@@ -20,6 +20,7 @@ namespace Practica3
         private ISimioProject proyectoApi;
         private string rutabase = "[MYS1]ModeloBase_P20.spfx";
         private string rutafinal = "[MYS1]ModeloFinal_P20.spfx";
+        private string rutafinal2 = "[MYS1]ModeloDatos_P20.spfx";
         private string[] warnings;
         int contNodos = 1;
         int contConveyor = 0;
@@ -47,6 +48,7 @@ namespace Practica3
             crearRutas();
             dibujarContorno();
             //modelo.Facility.IntelligentObjects["Source1"].Properties["InterarrivalTime"].Value = "Random.Exponential(5)";
+            Console.WriteLine(warnings.Length);
 
             SimioProjectFactory.SaveProject(proyectoApi, rutafinal, out warnings);
         }
@@ -555,6 +557,147 @@ namespace Practica3
             modelo.Facility.IntelligentObjects[nombre].Properties["DrawnToScale"].Value = "False";
             modelo.Facility.IntelligentObjects[nombre].Properties["LogicalLength"].Value = distancia.ToString();
             modelo.Facility.IntelligentObjects[nombre].Properties["InitialDesiredSpeed"].Value = "37.2823"; // Mts por seg
+        }
+
+        public void dibujarCarnet1()
+        {
+            // 201503840
+            dibujarDos(0, 0);       // 2
+            dibujarCero(2, 0);      // 0
+            dibujarUno(4, 0);       // 1
+            dibujarCinco(5, 0);     // 5
+            dibujarCero(7, 0);      // 0
+            dibujarTres(9, 0);      // 3
+            dibujarOcho(11, 0);     // 8
+            dibujarCuatro(13, 0);   // 4
+            dibujarCero(15, 0);     // 0
+        }
+
+        public void dibujarCarnet2()
+        {
+            // 201503840
+            dibujarDos(0, 3);       // 2
+            dibujarCero(2, 3);      // 0
+            dibujarUno(4, 3);       // 1
+            dibujarCinco(5, 3);     // 5
+            dibujarCero(7, 3);      // 0
+            dibujarTres(9, 3);      // 3
+            dibujarSiete(11, 3);    // 7
+            dibujarCinco(13, 3);    // 5
+            dibujarCero(15, 3);     // 0
+        }
+
+        public void dibujarDos(int despX, int despY)
+        {
+            // ------- 2 -------
+            crearTransferNode("N" + contNodos, 0 + despX, 0 + despY);
+            crearTransferNode("N" + contNodos, 1 + despX, 0 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 1 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 0 + despX, 1 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 0 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+        }
+
+        public void dibujarCero(int despX, int despY)
+        {
+            // ------- 0 -------
+            crearTransferNode("N" + contNodos, 0 + despX, 0 + despY);
+            crearTransferNode("N" + contNodos, 1 + despX, 0 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 0 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearPath("N" + (contNodos - 1), "N" + (contNodos - 4));
+        }
+
+        public void dibujarUno(int despX, int despY)
+        {
+            // ------- 1 -------
+            crearTransferNode("N" + contNodos, 0 + despX, 0 + despY);
+            crearTransferNode("N" + contNodos, 0 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+        }
+
+        public void dibujarCinco(int despX, int despY)
+        {
+            // ------- 5 -------
+            crearTransferNode("N" + contNodos, 1 + despX, 0 + despY);
+            crearTransferNode("N" + contNodos, 0 + despX, 0 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 0 + despX, 1 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 1 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 0 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+        }
+
+        public void dibujarTres(int despX, int despY)
+        {
+            // ------- 3 -------
+            crearTransferNode("N" + contNodos, 0 + despX, 0 + despY);
+            crearTransferNode("N" + contNodos, 1 + despX, 0 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 1 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 0 + despX, 1 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 3), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 0 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+        }
+
+        public void dibujarSiete(int despX, int despY)
+        {
+            // ------- 7 -------
+            crearTransferNode("N" + contNodos, 0 + despX, 0 + despY);
+            crearTransferNode("N" + contNodos, 1 + despX, 0 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 1 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+        }
+
+        public void dibujarOcho(int despX, int despY)
+        {
+            // ------- 8 -------
+            crearTransferNode("N" + contNodos, 0 + despX, 0 + despY);
+            crearTransferNode("N" + contNodos, 1 + despX, 0 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 1 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 0 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 0 + despX, 1 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearPath("N" + (contNodos - 1), "N" + (contNodos - 6));
+            crearPath("N" + (contNodos - 1), "N" + (contNodos - 4));
+        }
+
+        public void dibujarCuatro(int despX, int despY)
+        {
+            // ------- 4 -------
+            crearTransferNode("N" + contNodos, 0 + despX, 0 + despY);
+            crearTransferNode("N" + contNodos, 0 + despX, 1 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 1 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 2 + despY);
+            crearPath("N" + (contNodos - 2), "N" + (contNodos - 1));
+            crearTransferNode("N" + contNodos, 1 + despX, 0 + despY);
+            crearPath("N" + (contNodos - 1), "N" + (contNodos - 3));
         }
     }
 }
